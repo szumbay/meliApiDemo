7174fc77ccc84af7042ae2418cb66aa2cb38ca79
szumbay <--get>
2019-08-28 17:23:55 -0300

    unused archives deleted

--- app/src/main/java/com/example/meliapisdemo/MainActivity.kt
+++ app/src/main/java/com/example/meliapisdemo/MainActivity.kt
@@ -7,9 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toolbar
-import androidx.fragment.app.FragmentManager
-import kotlinx.android.synthetic.main.activity_searchable.*
 
 class MainActivity : AppCompatActivity() {
 
